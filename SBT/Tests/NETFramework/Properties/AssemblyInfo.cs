4781e70af5f9bd0024c9dd9c66be2b4b8a2ce286
Jakub <kowalik.kub@gmail.com>
2019-01-25 21:25:35 +0100

    Moved project to another folder

--- SBT/Tests/NETFramework/Properties/AssemblyInfo.cs
+++ SBT/Tests/NETFramework/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NETFramework4.6.1_Tests")]
+[assembly: AssemblyTitle("NETFramework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NETFramework4.6.1_Tests")]
+[assembly: AssemblyProduct("NETFramework")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("29385221-aef8-45b4-9ef5-c5f62db6bdaf")]
+[assembly: Guid("2119b1aa-6427-44e1-86bd-cabc51546bee")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
